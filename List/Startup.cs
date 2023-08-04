21c9c3eb03cefb0a975f3361d50514cf41a33327
st1temizbi <st1temizbi@WKSL-PC069GN8>
2023-08-04 12:56:30 +0300

    Add project files.

---
+++ List/Startup.cs
@@ -0,0 +1,55 @@
+﻿using List.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace List
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<INTERNContext>(options =>
+                options.UseSqlServer("Server=dindapptr126-31.ptg.local;Database=INTERN;User Id=usr_intern;Password=%i1V@2E97Q49;TrustServerCertificate=True;"));
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=SdDatabases}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
